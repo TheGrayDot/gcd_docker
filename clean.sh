77ab96791c363537012bd9868baeead9ea54fdb2
thomaslaurenson <thomas@thomaslaurenson.com>
2022-06-09 11:03:37 +1200

    Update clean script.

--- clean.sh
+++ clean.sh
@@ -2,3 +2,11 @@
 
 docker-compose down
 docker system prune -a -f
+
+echo "[+] Do you wish to remove existing MySQL data?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) sudo rm -rf ./mysql_data; break;;
+        No ) exit;;
+    esac
+done
